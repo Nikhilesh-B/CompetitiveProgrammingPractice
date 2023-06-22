e75abb24d22c0104b02902cba1af1d14b95f400c
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-06-22 11:37:55 -0400

    Inverted binary tree

--- competitive programming questions/invert_binary_tree.py
+++ competitive programming questions/invert_binary_tree.py
@@ -4,5 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root == []:
+            return []
+        
+        def inversion(root):
+            if root.left == None and root.right==None:
+                return root
+            
+            else:
+                root.left, root.right  = inversion(root.right), inversion(root.left)
+
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    
