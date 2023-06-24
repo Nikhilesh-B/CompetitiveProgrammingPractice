ae37b639d812a3429296408e02e262a313c0cc74
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-06-24 08:52:46 -0400

    combination sum not completed and not working

--- competitive programming questions/combination_sum.py
+++ competitive programming questions/combination_sum.py
@@ -4,23 +4,13 @@ class Solution:
             return []
         sum_set = {tuple(candidate) for candidate in candidates}
         saved_sums_backward = {candidate:[[candidate]] for candidate in candidates}
+        new_sums = {candidate:[[candidate]] for candidate in candidates}
 
-        for num in candidates:
+        for num in range(min(candidates), target):
             for sum in saved_sums_backward:
-                new_sum = num+sum
-                if sum ==  target:
-                    return saved_sums_backward[target]
-                if sum>target:
-                    return []
-                for pattern in saved_sums_backward[sum]:
-                    new_pattern = pattern.append(num).sort()
-                    if new_sum in saved_sums_backward and new_pattern not in sum_set:
-                        saved_sums_backward.append(new_pattern)
-                        sum_set.add(new_pattern)
-                    else:
-                        saved_sums_backward[num] = [pattern.append(num).sort()]
-                        sum_set.add(new_pattern)
-
+                for 
+        
+        
 
 if __name__ == "__main__":
     s = Solution()
