5cc81af682d67be5558e1afc85f4f703cb4bcb9d
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-06-26 08:26:47 -0400

    fixed for largest kth elements

--- competitive programming questions/findkthlargestelement.py
+++ competitive programming questions/findkthlargestelement.py
@@ -2,47 +2,26 @@ from collections import Counter
 
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
-        
-        mn, mx = min(nums), max(nums)
+        mx = max(nums)
         freqs = Counter(nums)
-
         running_cnt = 0 
 
-        query = mn-1
+        query = mx+1
         while running_cnt<k:
-            query += 1
+            query -= 1
             if query in freqs: 
                 running_cnt += freqs[query]
         
         return query
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import random
 if __name__ == "__main__":
     s = Solution()
-    length = 10
-    k = 2
-    nums = [random.randint(int(-1*10e4),int(10e4)) for _ in range(length)]  
-    print("Numbers analyzing=", nums)
+    length = int(10)
+    k = 1
+    #nums = [random.randint(int(-1*10e4),int(10e4)) for _ in range(length)]  
+    nums = [1]
+    #nums = [1,2,2,2,2,2,2,3,4,5,6,7]
+    print("Numbers analyzing=", sorted(nums), "kth smallest=", str(sorted(nums)[-k]))
     ans = s.findKthLargest(nums=nums,k=k)
-    print(str(k)+"nd/th smallest element= "+str(ans))
-
-
-
-
-
+    print(str(k)+"nd/th largest element= "+str(ans))
