0633257c22764dee124a2eb46527654cdf88f7c5
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-08-27 11:48:16 -0400

    we're not going to stop we're going to keep going my friend nikhilesh

--- competitive programming questions/first_missing_positive.py
+++ competitive programming questions/first_missing_positive.py
@@ -1,7 +1,22 @@
 class Solution():
-    pass 
+    #if you have constant time you have to use the data that you're given to work in constant time for you 
+    #find workarounds, it's often through marking or something on those lines 
+    def firstMissingPositive(self, nums: list[int]) -> int:
+        for i, num in enumerate(nums):
+            if num<0: 
+                nums[i] = 0
+
+        for j, num in enumerate(nums):
+            if num!=0:
+                nums[num-1]*=-1
+
+        for k, num in enumerate(nums):
+            if k>0:
+                return k+1
+        
 
 
 if __name__ == "__main__":
     pass
+
 
