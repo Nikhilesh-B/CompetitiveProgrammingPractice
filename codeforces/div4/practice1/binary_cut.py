d0165693e4edfc025edc9891ac26aa79de1865e6
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-05-12 18:37:31 -0400

    pushing binary cut

--- codeforces/div4/practice1/binary_cut.py
+++ codeforces/div4/practice1/binary_cut.py
@@ -2,14 +2,15 @@ if __name__ == "__main__":
     t = int(input())
     binary_strings = []
     for _ in range(t):
-        n = int(input())
-        a = list(map(int, input().split()))
+        a = input()
         binary_strings.append(a)
-    
+
+    print(binary_strings)
     for binary_string in binary_strings:
         pieces = 1
         zero_one = False
         for i in range(1, len(binary_string)):
+            binary_strings
             if binary_string[i] > binary_string[i-1]:
                 if zero_one:
                     pieces += 1
