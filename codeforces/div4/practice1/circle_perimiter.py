bd7ffcd7bf148226c212336d14c6f14397dac069
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-05-18 18:30:42 -0400

    circle perimeter

--- codeforces/div4/practice1/circle_perimiter.py
+++ codeforces/div4/practice1/circle_perimiter.py
@@ -9,12 +9,11 @@ if __name__ == "__main__":
 
     for r in radii:
         total_pts = 0
-        for x in range(-r, r, 1):
+        for x in range(-r, r+1, 1):
             low_y = ((r**2 - x**2)**0.5)
             high_y = (((r+1)**2 - x**2)**0.5)
             pts = math.floor(high_y-low_y)
             if low_y - math.floor(low_y) == 0:
                 pts += 1
-
-            total_pts += pts*2
+            total_pts += (pts*2)
         print(total_pts)
