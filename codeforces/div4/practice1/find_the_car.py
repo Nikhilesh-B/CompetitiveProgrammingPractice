49c18861f2e8384c3a263954bd807b6fb6daa2eb
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-05-18 17:38:24 -0400

    finding the car

--- codeforces/div4/practice1/find_the_car.py
+++ codeforces/div4/practice1/find_the_car.py
@@ -1,20 +1,31 @@
 import bisect
 
 if __name__ == "__main__":
+    n_k_qs = []
+    arr_distances = []
+    arr_times = []
     t = int(input())
     for _ in range(t):
         n_k_q = input()
-        n, k, q = list(map(int, n_k_q.split()))
+        n_k_qs.append(n_k_q)
         distances = list(map(int, input().split()))
         times = list(map(int, input().split()))
         distances.insert(0, 0)
         times.insert(0, 0)
+        arr_distances.append(distances)
+        arr_times.append(times)
+
+
+    for i in range(t):
+        n_k_q = n_k_qs[i]
+        distances = arr_distances[i]
+        times = arr_times[i]
+        n, k, q = map(int, n_k_q.split())
         for i in range(q):
             if i == q-1:
                 end_str = "\n"
             else:
                 end_str = ""
-
             query_distance = int(input())
             right_idx = bisect.bisect_left(distances, query_distance)
             left_idx = right_idx - 1
