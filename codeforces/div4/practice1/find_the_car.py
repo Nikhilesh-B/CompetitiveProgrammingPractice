65d1acabe7d22d17df9e1dba13721fe49425ca93
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-05-18 17:58:05 -0400

    find the car

--- codeforces/div4/practice1/find_the_car.py
+++ codeforces/div4/practice1/find_the_car.py
@@ -4,29 +4,42 @@ if __name__ == "__main__":
     n_k_qs = []
     arr_distances = []
     arr_times = []
+    arr_queries = []
     t = int(input())
     for _ in range(t):
         n_k_q = input()
+        n, k, q = map(int, n_k_q.split())
         n_k_qs.append(n_k_q)
+        queries = []
         distances = list(map(int, input().split()))
         times = list(map(int, input().split()))
         distances.insert(0, 0)
         times.insert(0, 0)
         arr_distances.append(distances)
         arr_times.append(times)
+        for _ in range(q):
+            queries.append(int(input()))
+        arr_queries.append(queries)
 
 
-    for i in range(t):
-        n_k_q = n_k_qs[i]
-        distances = arr_distances[i]
-        times = arr_times[i]
+    # print('This is n_k_qs', n_k_qs)
+    # print('This is arr_distances', arr_distances)
+    # print('This is arr_times', arr_times)
+    # print('This is arr_queries', arr_queries)
+    # print('This is t', t)   
+
+    for j in range(t):
+        n_k_q = n_k_qs[j]
+        distances = arr_distances[j]
+        times = arr_times[j]
+        queries = arr_queries[j]
         n, k, q = map(int, n_k_q.split())
         for i in range(q):
             if i == q-1:
                 end_str = "\n"
             else:
-                end_str = ""
-            query_distance = int(input())
+                end_str = " "
+            query_distance = queries[i]
             right_idx = bisect.bisect_left(distances, query_distance)
             left_idx = right_idx - 1
             if distances[right_idx] == query_distance:
@@ -38,4 +51,4 @@ if __name__ == "__main__":
                     distances[left_idx] - distances[right_idx]) / (times[left_idx] - times[right_idx])
                 time_to_reach_from_left_idx = (
                     query_distance - distances[left_idx]) / speed_between_points
-                print(int(times[left_idx] + time_to_reach_from_left_idx))
+                print(int(times[left_idx] + time_to_reach_from_left_idx), end=end_str)
