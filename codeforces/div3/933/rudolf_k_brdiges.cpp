f3400e134d59076c939a9eafe1f4baf04abbeea1
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-06-30 17:40:36 -0400

    the k bridges are not working

--- codeforces/div3/933/rudolf_k_brdiges.cpp
+++ codeforces/div3/933/rudolf_k_brdiges.cpp
@@ -26,61 +26,87 @@ void print_deque(deque<ipair> state)
     }
     cout << "]" << endl;
 }
-
 int process_sub_case(int m, int d, vector<int> &ci)
 {
     deque<ipair> state{};
-    for (int i = 0; i < m; ++i)
+    vector<int> dp(m, INT_MAX);
+    dp[0] = 1 + ci[0];
+    state.push_back(make_pair(dp[0], 0));
+
+    for (int i = 1; i < m; ++i)
     {
-        // cout << "P1 " << endl;
-        // print_deque(state);
-
-        // cout << "INDEX" << i << endl;
-        if (i == 0)
+        // Remove elements outside the window of size `d`
+        while (!state.empty() && i - state.front().second - 1 > d)
         {
-            state.push_back(make_pair(1, 0));
-            continue;
-        }
-        ipair topvalue = state[0];
-        int val = topvalue.first;
-        int idx = topvalue.second;
-        int newval = val + (ci[i] + 1);
-
-        if (abs(i - idx) - 1 == d)
             state.pop_front();
+        }
 
-        while (!state.empty() && state[state.size() - 1].first >= newval)
+        dp[i] = state.front().first + 1 + ci[i];
+
+        while (!state.empty() && state.back().first >= dp[i])
+        {
             state.pop_back();
+        }
 
-        state.push_back(make_pair(newval, i));
-        // // cout << "P2 " << endl;
-        // print_deque(state);
+        state.push_back(make_pair(dp[i], i));
     }
-    while (state[0].second != m - 1)
-        state.pop_front();
 
-    return state[0].first;
+    return dp[m - 1];
 }
+
+// int process_sub_case(int m, int d, vector<int> &ci)
+// {
+//     deque<ipair> state{};
+//     for (int i = 0; i < m; ++i)
+//     {
+//         // cout << "P1 " << endl;
+//         // print_deque(state);
+
+//         // cout << "INDEX" << i << endl;
+//         if (i == 0)
+//         {
+//             state.push_back(make_pair(1, 0));
+//             continue;
+//         }
+//         ipair topvalue = state[0];
+//         int val = topvalue.first;
+//         int idx = topvalue.second;
+//         int newval = val + (ci[i] + 1);
+
+//         if (abs(i - idx) - 1 == d)
+//             state.pop_front();
+
+//         while (!state.empty() && state[state.size() - 1].first >= newval)
+//             state.pop_back();
+
+//         state.push_back(make_pair(newval, i));
+//         // // cout << "P2 " << endl;
+//         // print_deque(state);
+//     }
+//     while (state[0].second != m - 1)
+//         state.pop_front();
+
+//     return state[0].first;
+// }
 
 int process_case(int n, int m, int k, int d, vector<vector<int>> &c)
 {
-    deque<int> answers{};
+    vector<int> answers{};
 
     for (int i = 0; i < n; ++i)
     {
         vector<int> cse = c[i];
         int val1 = process_sub_case(m, d, cse);
-        // reverse(cse.begin(), cse.end());
-        // int val2 = process_sub_case(m, d, cse);
-        answers.push_back(val1);
+        reverse(cse.begin(), cse.end());
+        int val2 = process_sub_case(m, d, cse);
+        answers.push_back(min(val1, val2));
     }
 
     int rval = 0;
     sort(answers.begin(), answers.end());
-    while (k--)
+    for (int i = 0; i < k; ++i)
     {
-        rval += answers[0];
-        answers.pop_front();
+        rval += answers[i];
     }
     return rval;
 }
