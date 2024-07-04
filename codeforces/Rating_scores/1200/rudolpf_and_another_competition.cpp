84636c2faae1b06788729466f3d27dc1f6d3bf8f
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-07-04 13:39:09 -0400

    first short of rudolf and aother competition

---
+++ codeforces/Rating_scores/1200/rudolpf_and_another_competition.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <map>
+#include <set>
+#include <climits>
+#include <queue>
+using namespace std;
+
+typedef pair<int, int> ipair;
+
+template <typename T>
+void printVectorNL(const std::vector<T> &vec)
+{
+    for (const auto &elem : vec)
+    {
+        std::cout << std::endl;
+    }
+}
+
+int solve()
+{
+    int n, m, h;
+    cin >> n >> m >> h;
+
+    vector<vector<int>> t(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            cin >> t[i][j];
+
+    vector<ipair> pp(n, {0, 0});
+
+    for (int i = 0; i < n; ++i)
+    {
+        vector<int> problems = t[i];
+        sort(problems.begin(), problems.end());
+        int penalty = 0;
+        int solved = 0;
+        for (auto p : problems)
+        {
+            if (penalty + p <= h)
+            {
+                solved++;
+                penalty += p;
+            }
+            else
+                break;
+        }
+        pp[i] = make_pair(solved, penalty);
+    }
+
+    ipair ru_score = pp[0];
+
+    auto sort_ipair = [](ipair p1, ipair p2)
+    {
+        if (p1.first != p2.first)
+            return p1.first > p2.first;
+        else
+            return p1.second < p2.second;
+    };
+
+    sort(pp.begin(), pp.end(), sort_ipair);
+
+    int ridx = -1;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (pp[i] == ru_score)
+            ridx = i;
+    }
+
+    return ridx;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    vector<int> places(t);
+    for (int i = 0; i < t; i++)
+        places[i] = solve();
+    printVectorNL(places);
+}
