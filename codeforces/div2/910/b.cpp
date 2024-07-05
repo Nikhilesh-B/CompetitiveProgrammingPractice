7968882eeacd140d3e7c78953bc5fd0afe3c23b8
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-07-05 08:25:54 -0400

    milena and admirer b.cpp looks correct

--- codeforces/div2/910/b.cpp
+++ codeforces/div2/910/b.cpp
@@ -33,10 +33,12 @@ int solve()
         double divnto = static_cast<double>(a[i]) / static_cast<double>(cmax);
         int divntoi = a[i] / cmax;
 
-        if (divnto > 2.0)
+        if (!(a[i] % cmax))
+            total_splits += divntoi - 1;
+        else if (divnto > 2.0)
         {
-            total_splits += divntoi - 2 + 1;
-            int lftover = a[i] - (divntoi - 2) * cmax;
+            total_splits += divntoi - 1 + 1;
+            int lftover = a[i] - (divntoi - 1) * cmax;
             cmax = min(lftover / 2, lftover - (lftover / 2));
         }
         else if (divnto > 1.0)
