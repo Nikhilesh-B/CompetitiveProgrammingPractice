16a50e3f4a8616ed7673c76cb997d2132860b188
Nikhilesh Belulkar <64567233+Nikhilesh-B@users.noreply.github.com>
2024-07-05 08:37:05 -0400

    why not use long long for everything integer related?

--- codeforces/div2/910/b.cpp
+++ codeforces/div2/910/b.cpp
@@ -14,32 +14,34 @@
 #include <queue>
 using namespace std;
 
-int solve()
+typedef long long ll;
+
+ll solve()
 {
     int n;
     cin >> n;
 
-    vector<int> a(n, 0);
+    vector<ll> a(n);
     for (int i = 0; i < n; ++i)
         cin >> a[i];
 
     reverse(a.begin(), a.end());
 
-    int total_splits = 0;
-    int cmax = a[0];
+    ll total_splits = 0;
+    ll cmax = a[0];
 
     for (int i = 1; i < n; ++i)
     {
         double divnto = static_cast<double>(a[i]) / static_cast<double>(cmax);
-        int divntoi = a[i] / cmax;
+        ll divntoi = a[i] / cmax;
 
         if (!(a[i] % cmax))
             total_splits += divntoi - 1;
         else if (divnto > 2.0)
         {
             total_splits += divntoi - 1 + 1;
-            int lftover = a[i] - (divntoi - 1) * cmax;
-            cmax = min(lftover / 2, lftover - (lftover / 2));
+            ll leftover = a[i] - (divntoi - 1) * cmax;
+            cmax = min(leftover / 2, leftover - (leftover / 2));
         }
         else if (divnto > 1.0)
         {
@@ -47,19 +49,26 @@ int solve()
             cmax = min(a[i] / 2, a[i] - (a[i] / 2));
         }
         else
+        {
             cmax = a[i];
+        }
     }
     return total_splits;
 }
 
 int main()
 {
+    ios::sync_with_stdio(false); // Disable synchronization with C I/O
+    cin.tie(nullptr);            // Untie cin from cout for faster input
+
     int t;
     cin >> t;
-    vector<int> answers(t, 0);
+    vector<ll> answers(t);
     for (int i = 0; i < t; ++i)
         answers[i] = solve();
 
     for (auto ans : answers)
         cout << ans << endl;
+
+    return 0;
 }
